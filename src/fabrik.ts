9171dcf55b1910f6690b18374ea72179e95d0335
Travis Stodter <travis.stodter@sharpnotions.com>
2019-08-17 11:21:47 -0400

    Trial fabrik step-by-step generator

--- src/fabrik.ts
+++ src/fabrik.ts
@@ -123,3 +123,86 @@ export const fabrik = (
   return jointPositions;
 };
 
+export const fabrikGenerator = (
+  tolerance: number,
+  maxIterations: Number = Infinity,
+  isRootFixed: boolean = true
+) => function* (
+  jointPositions: Array<Point>,
+  boneDistances: Array<number>,
+  target: Point
+): Iterator<Array<Point>> {
+  const numJoints = jointPositions.length;
+
+  // Distance between root and target
+  const rootToTarget = distance(jointPositions[0], target);
+
+  // Check whether the target is within reach
+  if (isRootFixed && rootToTarget > sum(boneDistances)) {
+    // The target is unreachable
+    for (let i = 0; i < boneDistances.length; i++) {
+      // Find the distance r_i between the target and the jointPositions[i]
+      const distFromJointToTarget = distance(jointPositions[i], target);
+      // Find the new joint positions p_i
+      jointPositions[i + 1] = movePointAlongSegmentBy(
+        boneDistances[i] / distFromJointToTarget,
+        target,
+        jointPositions[i]
+      );
+
+      yield jointPositions;
+    }
+
+    return jointPositions;
+  }
+
+  // The target is reachable; thus, set as b the initial position of the joint jointPositions[0]
+  let initialRootPosition = jointPositions[0];
+  // Check whether the distance between the end effector last(jointPositions) and the target
+  // is greater than a tolerance
+  let iteration = 0;
+  let distFromEndToTarget = distance(last(jointPositions), target);
+
+  while (distFromEndToTarget > tolerance && iteration < maxIterations) {
+    // STAGE 1: FORWARD REACHING
+    // Set the end effector last(jointPositions) as target
+    jointPositions[jointPositions.length - 1] = target;
+    for (let i = jointPositions.length - 2; i >= 0; i--) {
+      // Find the distance r_i between the new joint position jointPositions[i + 1] and jointPositions[i]
+      const newBoneDistance = distance(jointPositions[i], jointPositions[i + 1]);
+
+      // Find the new jointPositions[i]
+      jointPositions[i] = movePointAlongSegmentBy(
+        boneDistances[i] / newBoneDistance,
+        jointPositions[i],
+        jointPositions[i + 1]
+      );
+
+      yield jointPositions;
+    }
+
+    // STAGE 2: BACKWARD REACHING
+    // Set the root jointPositions[0] its initial position
+    if (isRootFixed) {
+      jointPositions[0] = initialRootPosition;
+    }
+
+    for (let i = 0; i < jointPositions.length - 1; i++) {
+      // Find the distance r_i between the new jointPositions[i] and jointPositions[i + 1]
+      const newBoneDistance = distance(jointPositions[i], jointPositions[i + 1]);
+      // Find the new jointPositions[i]
+      jointPositions[i + 1] = movePointAlongSegmentBy(
+        boneDistances[i] / newBoneDistance,
+        jointPositions[i + 1],
+        jointPositions[i]
+      );
+
+      yield jointPositions;
+    }
+
+    distFromEndToTarget = distance(last(jointPositions), target);
+    iteration += 1;
+  }
+
+  return jointPositions;
+};
